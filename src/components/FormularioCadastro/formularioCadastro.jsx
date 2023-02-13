1916a000fa9ec9e71a0b5d0f93df73774124936a
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-02-13 20:00:40 -0300

    Hooks e o useState

--- src/components/FormularioCadastro/formularioCadastro.jsx
+++ src/components/FormularioCadastro/formularioCadastro.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, Button, Switch, FormControlLabel } from '@mui/material'
 
 function FormularioCadastro() {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [CPF, setCPF] = useState("");
+
 
     return (
-        <form>
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                console.log(nome, sobrenome);
+            }}
+        >
             <TextField
+                value={nome}
+                onChange={(event) => {
+                    let tmpNome =event.target.value;
+                    if (tmpNome.length >= 3) {
+                        tmpNome = tmpNome.substring(0, 3);
+                    }
+                    setNome(tmpNome);
+                   
+                }}
+
+
                 id="nome"
                 label="Nome"
                 variant="outlined"
@@ -13,6 +33,12 @@ function FormularioCadastro() {
                 fullWidth
             />
             <TextField
+                value={sobrenome}
+                onChange={(event) => {
+                    setSobrenome(event.target.value);
+                }}
+
+
                 id="sobrenome"
                 label="Sobrenome"
                 variant="outlined"
@@ -20,6 +46,15 @@ function FormularioCadastro() {
                 fullWidth
             />
             <TextField
+                value={nome}
+                onChange={(event) => {
+                    setNome(event.target.value);
+                    if (nome.length > 3) {
+                        setNome(nome.substring(0, 3));
+                    }
+                }}
+
+
                 id="CPF"
                 label="CPF"
                 variant="outlined"
@@ -47,3 +82,11 @@ function FormularioCadastro() {
 }
 
 export default FormularioCadastro;
+
+/*
+const [sobrenome,setSobrenome] = useState(""); ==
+    const arr = useState("");
+    const sobrenome = arr[0];
+    const setSobrenome = arr[1];
+
+*/
