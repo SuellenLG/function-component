64428a1c5ac52f311962788a67c2227821ba378b
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-02-15 10:29:25 -0300

    Switch case

--- src/components/FormularioCadastro/Dadosusuario.jsx
+++ src/components/FormularioCadastro/Dadosusuario.jsx
@@ -1,13 +1,18 @@
 import { Button, TextField } from "@mui/material";
 import React from "react";
 
-function Dadosusuario() {
+function Dadosusuario({ aoEnviar }) {
     return (
-        <form>
+        <form onSubmit = {(event) => {
+            event.preventDefault();
+            aoEnviar()
+        }}>
+       
             <TextField
                 id="email"
                 label="email"
                 type="email"
+                required
                 variant="outlined"
                 margin="normal"
                 fullWidth />
@@ -15,6 +20,7 @@ function Dadosusuario() {
                 id="senha"
                 label="senha"
                 type="password"
+                required
                 variant="outlined"
                 margin="normal"
                 fullWidth />
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import Dadosusuario from './Dadosusuario';
-
+import { Typography } from '@mui/material';
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function proximo(){
+    setEtapaAtual(etapaAtual+1);
+  }
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <Dadosusuario aoEnviar={proximo}/>
+      case 1:
+        return < DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
+      case 2:
+        return <DadosEntrega aoEnviar={aoEnviar}/>
+      default:
+        return <Typography>Erro</Typography>
+    }
+  }
 
   return (
     <>
-      <DadosPessoais aoEnviar = {aoEnviar} validarCPF = {validarCPF}/>
-      <Dadosusuario />
-      <DadosEntrega />
+      {formularioAtual(etapaAtual)}
+
+
     </>
 
 
   );
 }
 
+
 export default FormularioCadastro;
 
+/*
+<DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+<Dadosusuario />
+<DadosEntrega />
+
+*/
